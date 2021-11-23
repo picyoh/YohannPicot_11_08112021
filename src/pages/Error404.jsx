bf30ecff6714a59a54a6524ed9bcb1c9f04e4188
picyoh <picotyoh@gmail.com>
2021-11-23 16:31:32 +0100

    mixins

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 class Error404 extends React.Component {
     render(){
          return (
-              <section>
+              <section className="error">
                    <h2>404</h2>
-                   <h3>Oups! La page que vous demandez n'existe pas.</h3>
+                   <div className="errorText">
+                   <span>Oups! La page que&nbsp;</span>
+                   <span>vous demandez n'existe pas.</span>
+                   </div>
                    <Link to='/'>Retourner sur la page d'acceuil</Link>
               </section>
          )
